60a59dcc1b1d01485761c99513386953b190e239
Sachin Barali <baralisachin5925@gmail.com>
2026-04-18 14:08:08 +0545

    urls file added

--- backend/tenants/urls.py
+++ backend/tenants/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TenantLoginView
+from .views import TenantLoginView,ListCreateTenantView
 
 urlpatterns = [
     path('login/',TenantLoginView.as_view(), name='tenant-login'),
+    path('list-create/',ListCreateTenantView.as_view(), name="list_create_tenant"),
 ]
